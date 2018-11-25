027ee15660090570e12d88505b764ec5945c9db0
Vinicius Yamauchi <vs.souza@gmail.com>
2018-11-25 11:42:24 +0000

    Created project structure.

---
+++ src/main/java/com/example/demo/spring/petclinic/DemoSpringPetClinicApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo.spring.petclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSpringPetClinicApplication.class, args);
+    }
+}
