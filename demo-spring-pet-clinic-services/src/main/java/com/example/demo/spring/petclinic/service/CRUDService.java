274b7cabc7ade01649b52a48aa12da9f26cab787
Vinicius Yamauchi <vs.souza@gmail.com>
2018-11-28 16:36:38 +0000

    Refactored services interface to use common base interface. Closes #11

---
+++ demo-spring-pet-clinic-services/src/main/java/com/example/demo/spring/petclinic/service/CRUDService.java
@@ -0,0 +1,11 @@
+package com.example.demo.spring.petclinic.service;
+
+import model.BaseEntity;
+
+import java.util.Set;
+
+public interface CRUDService<T extends BaseEntity, I> {
+    T findById(I id);
+    T save(T owner);
+    Set<T> findAll();
+}
--- demo-spring-pet-clinic-services/src/main/java/com/example/demo/spring/petclinic/service/VetService.java
+++ demo-spring-pet-clinic-services/src/main/java/com/example/demo/spring/petclinic/service/VetService.java
@@ -2,12 +2,6 @@ package com.example.demo.spring.petclinic.service;
 
 import model.Vet;
 
-import java.util.Set;
-
-public interface VetService {
-
-    Vet findById(Long id);
-    Vet save(Vet owner);
-    Set<Vet> findAll();
+public interface VetService extends CRUDService<Vet, Long> {
 
 }
