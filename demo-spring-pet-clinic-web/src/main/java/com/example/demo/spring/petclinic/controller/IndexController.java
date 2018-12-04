b4858581c3a4817a4e9329563b4f9d3da6f1255c
Vinicius Yamauchi <vs.souza@gmail.com>
2018-12-04 16:50:30 +0000

    Created Thymeleaf controller and index page. Closes #9

---
+++ demo-spring-pet-clinic-web/src/main/java/com/example/demo/spring/petclinic/controller/IndexController.java
@@ -0,0 +1,13 @@
+package com.example.demo.spring.petclinic.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping(path = {"", "index", "/", "index.html"})
+    public String showIndex() {
+        return "index";
+    }
+}
