39b91d81f353c1b12f69dad687e24183105451ac
Vinicius Yamauchi <vs.souza@gmail.com>
2018-12-04 17:46:51 +0000

    Added Vet page index and controller. Closes #7

---
+++ demo-spring-pet-clinic-web/src/main/java/com/example/demo/spring/petclinic/controller/VetController.java
@@ -0,0 +1,13 @@
+package com.example.demo.spring.petclinic.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class VetController {
+
+    @RequestMapping(path={"/vets/index", "/vets", "/vets/index.html"})
+    public String getVets() {
+        return "vets/index";
+    }
+}
