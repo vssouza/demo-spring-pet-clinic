770b5ca5a40eb9bc9c140e3df3885ee690735fa6
Vinicius Yamauchi <vs.souza@gmail.com>
2018-12-07 14:49:39 +0000

    Added commentary on how to execute with autoconfig report and how to check on execution parameters through help.

--- demo-spring-pet-clinic-web/src/main/java/com/example/demo/spring/petclinic/DemoSpringPetClinicApplication.java
+++ demo-spring-pet-clinic-web/src/main/java/com/example/demo/spring/petclinic/DemoSpringPetClinicApplication.java
@@ -3,6 +3,14 @@ package com.example.demo.spring.petclinic;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*
+ * mvn spring-boot:help -Ddetail=true
+ *      shows detailed options for the execution
+ *
+ * mvn spring-boot:run -Drun.arguents=--debug
+ *      will start the springboot software displaying autoconfiguration report
+ */
+
 @SpringBootApplication
 public class DemoSpringPetClinicApplication {
 
