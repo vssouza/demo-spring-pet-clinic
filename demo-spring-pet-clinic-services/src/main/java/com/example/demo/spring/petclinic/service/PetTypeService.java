8c09cebba8461cac7dfee8438d8413d0203018cb
Vinicius Yamauchi <vs.souza@gmail.com>
2019-02-20 14:58:40 +0000

    Create PetType service and map implementation. Closes #30

---
+++ demo-spring-pet-clinic-services/src/main/java/com/example/demo/spring/petclinic/service/PetTypeService.java
@@ -0,0 +1,6 @@
+package com.example.demo.spring.petclinic.service;
+
+import com.example.demo.spring.petclinic.model.PetType;
+
+public interface PetTypeService extends CRUDService<PetType, Long> {
+}
---
+++ demo-spring-pet-clinic-services/src/main/java/com/example/demo/spring/petclinic/service/map/PetTypeMapService.java
@@ -0,0 +1,40 @@
+package com.example.demo.spring.petclinic.service.map;
+
+import com.example.demo.spring.petclinic.model.PetType;
+import com.example.demo.spring.petclinic.service.PetTypeService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class PetTypeMapService extends AbstractMapService<PetType, Long> implements PetTypeService {
+
+    public PetTypeMapService() {
+        super();
+    }
+
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        super.delete(object);
+    }
+}
