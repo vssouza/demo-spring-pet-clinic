1491cdcc9a42c363fd4c62468116fc92b10d690d
Vinicius Yamauchi <vs.souza@gmail.com>
2019-02-20 17:20:32 +0000

    Added Speciality Service interface and map implementation. Closes #32

---
+++ demo-spring-pet-clinic-services/src/main/java/com/example/demo/spring/petclinic/service/SpecialityService.java
@@ -0,0 +1,6 @@
+package com.example.demo.spring.petclinic.service;
+
+import com.example.demo.spring.petclinic.model.Speciality;
+
+public interface SpecialityService extends CRUDService<Speciality, Long> {
+}
---
+++ demo-spring-pet-clinic-services/src/main/java/com/example/demo/spring/petclinic/service/map/SpecialityMapService.java
@@ -0,0 +1,40 @@
+package com.example.demo.spring.petclinic.service.map;
+
+import com.example.demo.spring.petclinic.model.Speciality;
+import com.example.demo.spring.petclinic.service.SpecialityService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class SpecialityMapService extends AbstractMapService<Speciality, Long> implements SpecialityService {
+
+    public SpecialityMapService() {
+        super();
+    }
+
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        super.delete(object);
+    }
+}
