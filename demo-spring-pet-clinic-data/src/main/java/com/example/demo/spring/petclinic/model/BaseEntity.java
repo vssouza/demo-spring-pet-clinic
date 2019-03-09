591b749937463f5cfe0447314163edd06c20e7c9
Vinicius Yamauchi <vs.souza@gmail.com>
2019-03-09 23:37:15 +0000

    Added JPA support for BaseEntity class. Closes #36

--- demo-spring-pet-clinic-data/src/main/java/com/example/demo/spring/petclinic/model/BaseEntity.java
+++ demo-spring-pet-clinic-data/src/main/java/com/example/demo/spring/petclinic/model/BaseEntity.java
@@ -3,12 +3,19 @@ package com.example.demo.spring.petclinic.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 
     private static final long serialVersionUID = 2493833670365380258L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Setter @Getter
     private Long id;
 
