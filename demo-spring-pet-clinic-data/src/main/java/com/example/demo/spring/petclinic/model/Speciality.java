05abcf794817312b9788b58628861c35ecd1fc3b
Vinicius Yamauchi <vs.souza@gmail.com>
2019-03-10 00:03:32 +0000

    Added JPA support for Vet entity. Closes #38

--- demo-spring-pet-clinic-data/src/main/java/com/example/demo/spring/petclinic/model/Speciality.java
+++ demo-spring-pet-clinic-data/src/main/java/com/example/demo/spring/petclinic/model/Speciality.java
@@ -3,8 +3,16 @@ package com.example.demo.spring.petclinic.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "speciality")
 public class Speciality extends BaseEntity {
 
+    @Column(name = "description")
     @Getter @Setter
     private String description;
 
--- demo-spring-pet-clinic-data/src/main/java/com/example/demo/spring/petclinic/model/Vet.java
+++ demo-spring-pet-clinic-data/src/main/java/com/example/demo/spring/petclinic/model/Vet.java
@@ -3,11 +3,16 @@ package com.example.demo.spring.petclinic.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "vets")
 public class Vet extends Person{
 
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "vet_specialties", joinColumns = @JoinColumn(name = "vet_id"), inverseJoinColumns = @JoinColumn(name = "speciality_id"))
     @Getter @Setter
     private Set<Speciality> specialities;
 
