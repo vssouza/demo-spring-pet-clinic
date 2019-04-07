ccf1faa0a493245b4841a0e57809bd69fdc3ee9a
Vinicius Yamauchi <vs.souza@gmail.com>
2019-04-07 14:21:24 +0100

    Changed controller test to use MockMvc.

--- demo-spring-pet-clinic-web/src/test/java/com/example/demo/spring/petclinic/controller/OwnerControllerTest.java
+++ demo-spring-pet-clinic-web/src/test/java/com/example/demo/spring/petclinic/controller/OwnerControllerTest.java
@@ -4,46 +4,43 @@ import com.example.demo.spring.petclinic.model.Owner;
 import com.example.demo.spring.petclinic.service.OwnerService;
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.ArgumentCaptor;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-import org.springframework.ui.Model;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.util.HashSet;
 import java.util.Set;
 
-import static org.junit.Assert.assertEquals;
-import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.*;
+import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+@RunWith(SpringRunner.class)
 public class OwnerControllerTest {
-
-    private static final String FIRST_OWNER_FIRST_NAME = "Vinicius";
-    private static final String FIRST_OWNER_LAST_NAME = "Yamauchi";
-
-    private static final String SECOND_OWNER_FIRST_NAME = "Bruno";
-    private static final String SECOND_OWNER_LAST_NAME = "Noda";
-
-    @Mock
-    private Model model;
 
     @Mock
     private OwnerService ownerService;
 
+    @InjectMocks
+    private OwnerController ownerController;
+
     @Mock
     private Owner firstMockOwner;
 
-    @Mock Owner secondMockOwner;
+    @Mock
+    private Owner secondMockOwner;
 
-    private OwnerController ownerController;
     private Set<Owner> ownerSet;
+
+    private MockMvc mockMvc;
 
     @Before
     public void setUp() throws Exception {
-        MockitoAnnotations.initMocks(this);
-        ownerController = new OwnerController(ownerService);
-
-        createOwnerMocks();
+        mockMvc = MockMvcBuilders.standaloneSetup(ownerController).build();
 
         ownerSet = new HashSet<>();
         ownerSet.add(firstMockOwner);
@@ -52,24 +49,12 @@ public class OwnerControllerTest {
         when(ownerService.findAll()).thenReturn(ownerSet);
     }
 
-    private void createOwnerMocks() {
-        when(firstMockOwner.getId()).thenReturn(1L);
-        when(firstMockOwner.getFirstName()).thenReturn(FIRST_OWNER_FIRST_NAME);
-        when(firstMockOwner.getLastName()).thenReturn(FIRST_OWNER_LAST_NAME);
-
-        when(secondMockOwner.getId()).thenReturn(2L);
-        when(secondMockOwner.getFirstName()).thenReturn(SECOND_OWNER_FIRST_NAME);
-        when(secondMockOwner.getLastName()).thenReturn(SECOND_OWNER_LAST_NAME);
-    }
-
     @Test
-    public void getOwnersPage() {
-        assertEquals("owners/index", ownerController.listOwners(model));
-        verify(ownerService, times(1)).findAll();
+    public void getOwnersPage() throws Exception {
+        mockMvc.perform(get("/owners/index"))
+                .andExpect(status().isOk())
+        .andExpect(view().name("owners/index"))
+        .andExpect(model().attribute("owners", hasSize(2)));
 
-        ArgumentCaptor<Set<Owner>> argumentCaptor = ArgumentCaptor.forClass(Set.class);
-
-        verify(model, times(1)).addAttribute(eq("owners"), argumentCaptor.capture());
-        assertEquals(2, argumentCaptor.getValue().size());
     }
 }
