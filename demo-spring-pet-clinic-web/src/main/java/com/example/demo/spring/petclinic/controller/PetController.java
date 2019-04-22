fe76bee59e43d2dbd0336d119ac67c4d61895aa0
Vinicius Yamauchi <vs.souza@gmail.com>
2019-04-22 18:26:13 +0100

    Added model attributes for pet controller. #57

--- demo-spring-pet-clinic-web/src/main/java/com/example/demo/spring/petclinic/controller/PetController.java
+++ demo-spring-pet-clinic-web/src/main/java/com/example/demo/spring/petclinic/controller/PetController.java
@@ -1,25 +1,49 @@
 package com.example.demo.spring.petclinic.controller;
 
+import com.example.demo.spring.petclinic.model.Owner;
+import com.example.demo.spring.petclinic.model.PetType;
+import com.example.demo.spring.petclinic.service.OwnerService;
 import com.example.demo.spring.petclinic.service.PetService;
+import com.example.demo.spring.petclinic.service.PetTypeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Collection;
+
 @Controller
-@RequestMapping("/pets")
+@RequestMapping("/owners/{ownerId}/pets")
 public class PetController {
 
     private final PetService petService;
+    private final PetTypeService petTypeService;
+    private final OwnerService ownerService;
 
     @Autowired
-    public PetController(final PetService petService) {
+    public PetController(final PetService petService, final PetTypeService petTypeService, final OwnerService ownerService) {
         this.petService = petService;
+        this.petTypeService = petTypeService;
+        this.ownerService = ownerService;
     }
 
-    @RequestMapping(path={"", "/index", "/index.html"})
-    public String getIndex(final Model model) {
-
-        return "pets/index";
+    @InitBinder("owner")
+    public void initOwnerBinder(WebDataBinder dataBinder) {
+        dataBinder.setDisallowedFields("id");
     }
+
+    @ModelAttribute("types")
+    public Collection<PetType> populatePetTypes() {
+        return this.petTypeService.findAll();
+    }
+
+    @ModelAttribute("owner")
+    public Owner findOwner(@PathVariable final Long ownerId) {
+        return this.ownerService.findById(ownerId);
+    }
+
+
 }
