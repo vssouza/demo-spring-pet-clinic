5293e74b5eaf32394e9b9c98c4560722ce0975be
vini.souza <vini.souza@workday.com>
2019-05-11 15:18:43 +0100

    added JSON api for vet list and fixed layout to use different url to retrieve page

--- demo-spring-pet-clinic-web/src/main/java/com/example/demo/spring/petclinic/controller/VetController.java
+++ demo-spring-pet-clinic-web/src/main/java/com/example/demo/spring/petclinic/controller/VetController.java
@@ -1,10 +1,16 @@
 package com.example.demo.spring.petclinic.controller;
 
-import com.example.demo.spring.petclinic.service.VetService;
+import java.util.Set;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.example.demo.spring.petclinic.model.Vet;
+import com.example.demo.spring.petclinic.service.VetService;
 
 @Controller
 @RequestMapping("/vets")
@@ -17,9 +23,14 @@ public class VetController {
         this.vetService = vetService;
     }
 
-    @RequestMapping(path={"", "/index", "/index.html"})
+    @RequestMapping(path = {"/index", "/index.html" })
     public String getVets(final Model model) {
         model.addAttribute("vets", vetService.findAll());
         return "vets/index";
+    }
+
+    @GetMapping(path = "")
+    public @ResponseBody Set<Vet> getVetsJson() {
+        return vetService.findAll();
     }
 }
