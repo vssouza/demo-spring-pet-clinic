f71481b22ce206751272f5cc3381d7650ad97e18
vini.souza <vini.souza@workday.com>
2019-05-11 15:07:25 +0100

    created vet list page. closes #62

--- demo-spring-pet-clinic-data/src/main/java/com/example/demo/spring/petclinic/model/Vet.java
+++ demo-spring-pet-clinic-data/src/main/java/com/example/demo/spring/petclinic/model/Vet.java
@@ -1,17 +1,24 @@
 package com.example.demo.spring.petclinic.model;
 
-import lombok.Getter;
-import lombok.Setter;
-
-import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Table(name = "vets")
 @Getter
 @Setter
-public class Vet extends Person{
+public class Vet
+        extends Person {
 
     @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(name = "vet_specialties", joinColumns = @JoinColumn(name = "vet_id"), inverseJoinColumns = @JoinColumn(name = "speciality_id"))
